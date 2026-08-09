0f74b2dae74cf52a0a9fb3eabb43d31aa36cb5ca
AbdurRahman <theboss87163@gmail.com>
2026-08-09 08:38:12 -0700

    Compelete Projects upto the Ass05

--- basicReact/src/components/Ass05.jsx
+++ basicReact/src/components/Ass05.jsx
@@ -5,6 +5,14 @@ function Ass05(){
     const [like, setLike] = useState(true);
     const [count, setCount] = useState(0);
     const [color, setColor] = useState("white");
+    const [bugCount, setBugCount] = useState(0);
+    
+    function bugCountFn(){
+        setBugCount(prev => prev + 1)
+        setBugCount(prev => prev + 1)
+        setBugCount(prev => prev + 1)
+    }
+
     function likeClicked(){
         setLike((prev) => !prev);
         setCount(count + 1)
@@ -12,9 +20,11 @@ function Ass05(){
     }
     return(
         <Main>
-            <h1>No : 5</h1>
+            <h1 className="text-5xl">No : 5</h1>
+            <br /><br />
             <button className="border-2 px-5" style={{ borderColor:color}} onClick={likeClicked}>Liked {count}</button>
-            <h3>Pehle hi challenge wala bna chuka hu chai aur code vala course jb dekha tha jo ab out dated he</h3>
+            <br /><br />
+            <h3 onClick={bugCountFn}>Count: {bugCount}</h3>
         </Main>
     )
 }
